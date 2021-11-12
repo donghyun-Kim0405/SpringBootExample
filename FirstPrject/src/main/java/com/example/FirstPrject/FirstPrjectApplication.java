02f69aebe31e16275392928d2206ecf9ffbc7259
donghyun's mac mini <donghyun-kim@donghyunsui-Macmini.local>
2021-11-12 18:08:04 +0900

    initialized

---
+++ FirstPrject/src/main/java/com/example/FirstPrject/FirstPrjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.FirstPrject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstPrjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstPrjectApplication.class, args);
+	}
+
+}
---
+++ FirstPrject/src/test/java/com/example/FirstPrject/FirstPrjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FirstPrject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstPrjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
